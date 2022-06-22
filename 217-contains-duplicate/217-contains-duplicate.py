d227624327848d465a7134a4dc34324cee9c4b51
Almas <62244354+almasfiza@users.noreply.github.com>
2022-06-22 10:47:08 +0530

    Time: 846 ms (12.41%), Space: 26 MB (29.91%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for i in nums:
+            if i in hashset:
+                return True
+            else:
+                hashset.add(i)
+                
+        return False
+        
