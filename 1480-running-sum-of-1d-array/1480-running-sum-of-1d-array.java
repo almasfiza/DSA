a8312f185a6bc2247752cf28c0e127164850806c
Almas <62244354+almasfiza@users.noreply.github.com>
2022-06-25 23:05:02 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (83.47%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -3,12 +3,12 @@ class Solution {
         //the sum array is as long as the nums array
         int sum[] = new int[nums.length];
         
-        for(int i = 0 ; i < nums.length; i ++){
-            int k = 0;
-            while(k<=i){
-                sum[i] += nums[k];
-                k++;
-            }
+        //initially the zeroeth element is the same
+        sum[0] = nums[0];
+        
+        //the sum at each index is like cumulative sum of the previous element of the sum[]
+        for(int i = 1; i < nums.length; i++){
+            sum[i] = sum[i-1] + nums[i];
         }
         
         return sum;
