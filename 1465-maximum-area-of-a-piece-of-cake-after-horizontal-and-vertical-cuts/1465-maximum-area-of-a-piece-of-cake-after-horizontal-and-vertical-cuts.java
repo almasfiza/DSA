0e8f5107f8c742ca03191c505f725089f8551b20
Almas <62244354+almasfiza@users.noreply.github.com>
2022-07-02 11:50:57 +0530

    Update 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -14,3 +14,33 @@ class Solution {
         return (int) ((long) maxX * (long)maxY % 1000000007L);
     }
 }
+/***
+class Solution {
+    public long maxDiff(int n, int arr[]){
+        long res = 0;
+        for(int i = 1; i < arr.length; i++){
+            res = Math.max(res, arr[i]-arr[i-1]);
+        }
+        return Math.max(n-arr[arr.length-1], res);
+    }
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        
+        //following the hints
+        long mod = (long)1e9+7;
+        //1. Sort the arrays
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        
+        //2. Finding the max diff bw the two consecutive elements for both the arrays.
+        //max difference
+        long maxH_diff = maxDiff(h, horizontalCuts);
+        long maxV_diff = maxDiff(w, verticalCuts);
+        
+        //3. Product of the two maxH_diff and maxV_diff is the answer.
+        return  (int) ((maxH_diff*maxV_diff)%mod);
+        
+    }
+}
+
+
+***/
