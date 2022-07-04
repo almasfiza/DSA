c35362311a404db513312f0b50eea876ac51980f
Almas <62244354+almasfiza@users.noreply.github.com>
2022-07-04 10:17:01 +0530

    Time: 8 ms (49.85%), Space: 47.2 MB (7.13%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -12,14 +12,21 @@ class Solution {
         //test 1- checking if the hm is created properly - works
         //System.out.println(hm);
         
+        //returning the result as an array of two indices who satisfy the two sum
         int[] res = new int[2];
         
+        
         for(int i = 0 ; i < nums.length; i++){
+            
             int a = nums[i];
+            //storing the index of the first element.
             res[0] = i;
+            
+            //subtracting from target to find if the other element exists in the hm and also to make sure that the current element is not considered.
             int find = target - a;
-            if(hm.containsKey(find) && hm.get(find)!=i){
+            if(hm.containsKey(find) && hm.get(find) != i){
                 res[1] = hm.get(find); 
+                //breaking as soon as the "exactly once" result is found.
                 break;
                 
             }
