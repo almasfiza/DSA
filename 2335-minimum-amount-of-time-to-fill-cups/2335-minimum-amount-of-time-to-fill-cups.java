be5b8542d4f2ba5f92cf65d4538c35a93138f566
Almas <62244354+almasfiza@users.noreply.github.com>
2022-07-10 10:20:47 +0530

    Time: 1 ms (100.00%), Space: 41.7 MB (55.56%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fillCups(int[] amount) {
+        Arrays.sort(amount);
+        int x = amount[0];
+        int y = amount[1];
+        int z = amount[2];
+        // 5 4 4 
+        if(x + y > z) return (x + y + z) / 2 + (x + y + z) % 2;
+        return z;
+    }
+}
